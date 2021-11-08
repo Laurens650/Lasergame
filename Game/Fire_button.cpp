dbbc8dcc2b369555fbb1942761d4e356b32f0af1
Laurens650 <74240768+Laurens650@users.noreply.github.com>
2021-11-08 13:17:01 +0100

    Fire_button
    The button to shoot

---
+++ Game/Fire_button.cpp
@@ -0,0 +1,26 @@
+#include "hwlib.hpp"
+#include "rtos.hpp"
+
+class Fire_button : rtos::task<>{
+    private:
+        schiet_control & s_control;
+        auto trigger = hwlib::target::pin_in(hwlib::target::pins::d23);
+        trigger.pullup_enable();
+        
+        void main(){
+            for(;;){
+                trigger.refresh();
+                if(trigger.read()==1){
+                    s_control.buttonPressed();
+                }else{
+                    s_control.buttonReleased();
+                }
+                hwlib::wait_ms(5);
+            }
+        }
+        
+    public:
+        Fire_button(schiet_control & s_control)
+            s_control(s_control)
+        {}
+};
