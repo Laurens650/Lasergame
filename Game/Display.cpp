06e8494a295ad9166cd2d2ef94f0f554b09b3e98
Jasper <jaspervdpost@gmail.com>
2021-11-09 11:40:42 +0100

    added char to int functions

--- Game/Display.cpp
+++ Game/Display.cpp
@@ -7,21 +7,58 @@ class display : public rtos::task<>{
         rtos::flag livesFlag;
         rtos::flag timeFlag;
         rtos::flag commandFlag;
-        auto scl = hwlib::target::pin_oc(hwlib::target::pins::scl);
-        auto sda = hwlib::target::pin_oc(hwlib::target::pins::sda);
-        auto bus = hwlib::i2c_bus_bit_banged_scl_sda(scl,sda);
-        auto oled = hwlib::glcd_oled(bus, 0x3c);
+        hwlib::target::pin_oc scl;
+        hwlib::target::pin_oc sda;
+        hwlib::i2c_bus_bit_banged_scl_sda bus;
+        hwlib::glcd_oled oled;
         oled.clear();
-        oled.flush();
+
         state_t state = IDLE;
         int Lives;
         int Seconds;
         int Time_min;
         int minutes;
         int display_seconds;
-        auto font    = hwlib::font_default_8x8();
-        auto Display = hwlib::terminal_from( oled, font );
-        
+        hwlib::font_default_8x8 font;
+        hwlib::terminal_from Display;
+
+        std::array<char, 2> two_ints_to_char(int a){
+            if (a < 10){
+                char eental = (char)a + '0';
+                std::array<char, 2> array = {'0', eental};
+                return array;
+            }
+            else{
+                float int_decimal = a / 10.0;
+                int left = (int)int_decimal;
+                int right = a % 10;
+
+                char tiental = (char)(left + '0');
+                char eental = (char)(right + '0');
+
+                std::array<char, 2> array = {tiental, eental};
+                return array;
+            }
+        }
+
+        std::array<char, 3> three_ints_to_char(int a){
+            if ( a < 100 ){
+                std::array<char, 2> b = two_ints_to_char(a);
+                std::array<char, 3> array = {'0',  b[0], b[1]};
+                return array;
+            }
+            else{
+                float int_decimal = a / 100.0;
+                int left = (int)int_decimal;
+                char honderdtal = (char)(left + '0');
+
+                int rest = (float)(int_decimal - left) * 100;
+                std::array<char, 2> b = two_ints_to_char(rest);
+                std::array<char, 3> array = {honderdtal, b[0], b[1]};
+                return array;
+            }
+
+        }
         
         void main(){
             for(;;){
@@ -34,9 +71,11 @@ class display : public rtos::task<>{
                             Display 
                                 << 
                                 << hwlib::flush;
-                        }else if(evt==timeFlag){
+                        }
+                        else if(evt==timeFlag){
                             
-                        }else if(evt==commandFlag){
+                        }
+                        else if(evt==commandFlag){
                             
                         }
                         oled.flush();
@@ -62,6 +101,12 @@ class display : public rtos::task<>{
             rtos::task(6, "display"),
             livesFlag(this, "livesFlag"),
             timeFlag(this, "timeFlag"),
-            commandFlag(this, "commandFlag")
+            commandFlag(this, "commandFlag"),
+            scl(hwlib::target::pins::scl),
+            sda(hwlib::target::pins::sda),
+            bus(scl, sda),
+            oled(bus, 0x3c),
+            font(),
+            Display( oled, font )
         {}
 };
